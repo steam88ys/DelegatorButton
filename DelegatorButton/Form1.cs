e24e3118c2f744de2b873fe37b6195254c07e961
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-08-31 15:39:31 +0900

    델리게이터를 활용한 버튼 이벤트핸들러 추가

--- DelegatorButton/Form1.cs
+++ DelegatorButton/Form1.cs
@@ -16,5 +16,23 @@ namespace DelegatorButton
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button button = new Button();
+            button.Text = "버튼";
+            button.Click += delegate (object s, EventArgs a)
+            {
+                MessageBox.Show("무명 델리게이터를 사용한 이벤트 연결");
+            };
+            EventHandler click = null;
+            click = (e2, a2) =>
+            {
+                MessageBox.Show("람다를 사용한 이벤트 연결");
+                button.Click -= click;
+            };
+            button.Click += click;
+            Controls.Add(button);
+        }
     }
 }
